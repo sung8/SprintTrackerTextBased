f56977cc1707a3f3239363973b5bc2b542817b39
NF09830453 <fyre.lord31@gmail.com>
2023-12-06 22:51:23 -0500

    Calendar

---
+++ CalendarView.Designer.cs
@@ -0,0 +1,491 @@
+﻿namespace SprintTrackerBasic
+{
+    partial class CalendarView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button2 = new Button();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            treeView1 = new TreeView();
+            treeView2 = new TreeView();
+            treeView3 = new TreeView();
+            treeView4 = new TreeView();
+            treeView5 = new TreeView();
+            treeView6 = new TreeView();
+            treeView7 = new TreeView();
+            treeView8 = new TreeView();
+            treeView9 = new TreeView();
+            treeView10 = new TreeView();
+            treeView11 = new TreeView();
+            treeView12 = new TreeView();
+            treeView13 = new TreeView();
+            treeView14 = new TreeView();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            label17 = new Label();
+            label18 = new Label();
+            label19 = new Label();
+            label20 = new Label();
+            label21 = new Label();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.Highlight;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = SystemColors.HighlightText;
+            button2.Location = new Point(159, 29);
+            button2.Name = "button2";
+            button2.Size = new Size(113, 46);
+            button2.TabIndex = 3;
+            button2.Text = "Add User";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaptionText;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.HighlightText;
+            button1.Location = new Point(25, 29);
+            button1.Name = "button1";
+            button1.Size = new Size(113, 46);
+            button1.TabIndex = 2;
+            button1.Text = "New Task";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(23, 174);
+            label1.Name = "label1";
+            label1.Size = new Size(53, 28);
+            label1.TabIndex = 19;
+            label1.Text = "Date";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(146, 174);
+            label2.Name = "label2";
+            label2.Size = new Size(53, 28);
+            label2.TabIndex = 20;
+            label2.Text = "Date";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(271, 174);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 28);
+            label3.TabIndex = 21;
+            label3.Text = "Date";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(399, 174);
+            label4.Name = "label4";
+            label4.Size = new Size(53, 28);
+            label4.TabIndex = 22;
+            label4.Text = "Date";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(526, 174);
+            label5.Name = "label5";
+            label5.Size = new Size(53, 28);
+            label5.TabIndex = 23;
+            label5.Text = "Date";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(650, 174);
+            label6.Name = "label6";
+            label6.Size = new Size(53, 28);
+            label6.TabIndex = 24;
+            label6.Text = "Date";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(779, 174);
+            label7.Name = "label7";
+            label7.Size = new Size(53, 28);
+            label7.TabIndex = 25;
+            label7.Text = "Date";
+            // 
+            // treeView1
+            // 
+            treeView1.Location = new Point(23, 205);
+            treeView1.Name = "treeView1";
+            treeView1.Size = new Size(104, 109);
+            treeView1.TabIndex = 26;
+            // 
+            // treeView2
+            // 
+            treeView2.Location = new Point(146, 205);
+            treeView2.Name = "treeView2";
+            treeView2.Size = new Size(104, 109);
+            treeView2.TabIndex = 27;
+            // 
+            // treeView3
+            // 
+            treeView3.Location = new Point(271, 205);
+            treeView3.Name = "treeView3";
+            treeView3.Size = new Size(104, 109);
+            treeView3.TabIndex = 28;
+            // 
+            // treeView4
+            // 
+            treeView4.Location = new Point(399, 205);
+            treeView4.Name = "treeView4";
+            treeView4.Size = new Size(104, 109);
+            treeView4.TabIndex = 29;
+            // 
+            // treeView5
+            // 
+            treeView5.Location = new Point(526, 205);
+            treeView5.Name = "treeView5";
+            treeView5.Size = new Size(104, 109);
+            treeView5.TabIndex = 30;
+            // 
+            // treeView6
+            // 
+            treeView6.Location = new Point(650, 205);
+            treeView6.Name = "treeView6";
+            treeView6.Size = new Size(104, 109);
+            treeView6.TabIndex = 31;
+            // 
+            // treeView7
+            // 
+            treeView7.Location = new Point(779, 205);
+            treeView7.Name = "treeView7";
+            treeView7.Size = new Size(104, 109);
+            treeView7.TabIndex = 32;
+            // 
+            // treeView8
+            // 
+            treeView8.Location = new Point(779, 378);
+            treeView8.Name = "treeView8";
+            treeView8.Size = new Size(104, 109);
+            treeView8.TabIndex = 39;
+            // 
+            // treeView9
+            // 
+            treeView9.Location = new Point(650, 378);
+            treeView9.Name = "treeView9";
+            treeView9.Size = new Size(104, 109);
+            treeView9.TabIndex = 38;
+            // 
+            // treeView10
+            // 
+            treeView10.Location = new Point(526, 378);
+            treeView10.Name = "treeView10";
+            treeView10.Size = new Size(104, 109);
+            treeView10.TabIndex = 37;
+            // 
+            // treeView11
+            // 
+            treeView11.Location = new Point(399, 378);
+            treeView11.Name = "treeView11";
+            treeView11.Size = new Size(104, 109);
+            treeView11.TabIndex = 36;
+            // 
+            // treeView12
+            // 
+            treeView12.Location = new Point(271, 378);
+            treeView12.Name = "treeView12";
+            treeView12.Size = new Size(104, 109);
+            treeView12.TabIndex = 35;
+            // 
+            // treeView13
+            // 
+            treeView13.Location = new Point(146, 378);
+            treeView13.Name = "treeView13";
+            treeView13.Size = new Size(104, 109);
+            treeView13.TabIndex = 34;
+            // 
+            // treeView14
+            // 
+            treeView14.Location = new Point(23, 378);
+            treeView14.Name = "treeView14";
+            treeView14.Size = new Size(104, 109);
+            treeView14.TabIndex = 33;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(23, 347);
+            label8.Name = "label8";
+            label8.Size = new Size(53, 28);
+            label8.TabIndex = 40;
+            label8.Text = "Date";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(146, 347);
+            label9.Name = "label9";
+            label9.Size = new Size(53, 28);
+            label9.TabIndex = 41;
+            label9.Text = "Date";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.Location = new Point(271, 347);
+            label10.Name = "label10";
+            label10.Size = new Size(53, 28);
+            label10.TabIndex = 42;
+            label10.Text = "Date";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label11.Location = new Point(399, 347);
+            label11.Name = "label11";
+            label11.Size = new Size(53, 28);
+            label11.TabIndex = 43;
+            label11.Text = "Date";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label12.Location = new Point(526, 347);
+            label12.Name = "label12";
+            label12.Size = new Size(53, 28);
+            label12.TabIndex = 44;
+            label12.Text = "Date";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label13.Location = new Point(650, 347);
+            label13.Name = "label13";
+            label13.Size = new Size(53, 28);
+            label13.TabIndex = 45;
+            label13.Text = "Date";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label14.Location = new Point(779, 347);
+            label14.Name = "label14";
+            label14.Size = new Size(53, 28);
+            label14.TabIndex = 46;
+            label14.Text = "Date";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label15.Location = new Point(25, 130);
+            label15.Name = "label15";
+            label15.Size = new Size(46, 28);
+            label15.TabIndex = 53;
+            label15.Text = "Day";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label16.Location = new Point(146, 130);
+            label16.Name = "label16";
+            label16.Size = new Size(46, 28);
+            label16.TabIndex = 52;
+            label16.Text = "Day";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label17.Location = new Point(271, 130);
+            label17.Name = "label17";
+            label17.Size = new Size(46, 28);
+            label17.TabIndex = 51;
+            label17.Text = "Day";
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label18.Location = new Point(399, 130);
+            label18.Name = "label18";
+            label18.Size = new Size(46, 28);
+            label18.TabIndex = 50;
+            label18.Text = "Day";
+            // 
+            // label19
+            // 
+            label19.AutoSize = true;
+            label19.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label19.Location = new Point(526, 130);
+            label19.Name = "label19";
+            label19.Size = new Size(46, 28);
+            label19.TabIndex = 49;
+            label19.Text = "Day";
+            // 
+            // label20
+            // 
+            label20.AutoSize = true;
+            label20.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label20.Location = new Point(650, 130);
+            label20.Name = "label20";
+            label20.Size = new Size(46, 28);
+            label20.TabIndex = 48;
+            label20.Text = "Day";
+            // 
+            // label21
+            // 
+            label21.AutoSize = true;
+            label21.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label21.Location = new Point(779, 130);
+            label21.Name = "label21";
+            label21.Size = new Size(46, 28);
+            label21.TabIndex = 47;
+            label21.Text = "Day";
+            // 
+            // CalendarView
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(901, 583);
+            Controls.Add(label15);
+            Controls.Add(label16);
+            Controls.Add(label17);
+            Controls.Add(label18);
+            Controls.Add(label19);
+            Controls.Add(label20);
+            Controls.Add(label21);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(treeView8);
+            Controls.Add(treeView9);
+            Controls.Add(treeView10);
+            Controls.Add(treeView11);
+            Controls.Add(treeView12);
+            Controls.Add(treeView13);
+            Controls.Add(treeView14);
+            Controls.Add(treeView7);
+            Controls.Add(treeView6);
+            Controls.Add(treeView5);
+            Controls.Add(treeView4);
+            Controls.Add(treeView3);
+            Controls.Add(treeView2);
+            Controls.Add(treeView1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "CalendarView";
+            Text = "CalendarView";
+            Load += CalendarView_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button2;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TreeView treeView1;
+        private TreeView treeView2;
+        private TreeView treeView3;
+        private TreeView treeView4;
+        private TreeView treeView5;
+        private TreeView treeView6;
+        private TreeView treeView7;
+        private TreeView treeView8;
+        private TreeView treeView9;
+        private TreeView treeView10;
+        private TreeView treeView11;
+        private TreeView treeView12;
+        private TreeView treeView13;
+        private TreeView treeView14;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private Label label17;
+        private Label label18;
+        private Label label19;
+        private Label label20;
+        private Label label21;
+    }
+}
---
+++ CalendarView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SprintTrackerBasic
+{
+    public partial class CalendarView : Form
+    {
+        Label[] labels = new Label[14];
+        Label[] dayLabels = new Label[7];
+
+        public CalendarView()
+        {
+            InitializeComponent();
+        }
+
+        private void CalendarView_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 14; i++)
+            {
+                labels[i] = (Label)this.Controls.Find("label" + (i + 1), true)[0];
+            }
+            for (int i = 0; i < 7; i++)
+            {
+                dayLabels[i] = (Label)this.Controls.Find("label" + (i + 15), true)[0];
+            }
+
+            DateTime currentDate = DateTime.Today;
+            for (int i = 0; i < 14; i++)
+            {
+                labels[i].Text = currentDate.AddDays(i).ToShortDateString();
+                if (i < 7)
+                {
+                    dayLabels[i].Text = currentDate.AddDays(i).DayOfWeek.ToString();
+                }
+            }
+        }
+    }
+}
