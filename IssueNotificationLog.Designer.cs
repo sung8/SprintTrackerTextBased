c2bcd6d582663bd9fe481ee1963b4b9c53f8bf71
sung <heechuw@gmail.com>
2023-12-09 18:26:04 -0500

    winform log view of notifications

---
+++ IssueNotificationLog.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace SprintTrackerBasic
+{
+    partial class IssueNotificationLog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 89);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(560, 360);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(267, 32);
+            label1.TabIndex = 1;
+            label1.Text = "Issue Notificion History";
+            // 
+            // IssueNotificationLog
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(584, 461);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "IssueNotificationLog";
+            Text = "IssueNotificationLog";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+    }
+}
